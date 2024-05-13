f4cb225e2a34daba0e58637469d537c9b54cd9f2
Danil <matrosovdan@mail.ru>
2024-05-13 18:35:28 +0300

    optimize imports

--- src/main/java/ru/matrosov/repository/PersonRepository.java
+++ src/main/java/ru/matrosov/repository/PersonRepository.java
@@ -4,8 +4,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import ru.matrosov.model.Person;
 
-import java.util.Optional;
-
 @Repository
 public interface PersonRepository extends JpaRepository<Person, String> {
 }
